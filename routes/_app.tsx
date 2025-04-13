3f20d65fb57a12c8d8cef6d663d7089cf4fe61cc
Lee Dogeon <dev.moreal@gmail.com>
2025-04-13 17:26:36 +0900

    refactor: remove unnecessary code

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,14 +1,13 @@
 import { type PageProps } from "$fresh/server.ts";
 import * as constants from "@/constants.ts";
 
-export default function ({ Component, state }: PageProps) {
-  const titleSuffix = state.subtitle ? ` - ${state.subtitle}` : "";
+export default function ({ Component }: PageProps) {
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{constants.TITLE}{titleSuffix}</title>
+        <title>{constants.TITLE}</title>
         <link rel="icon" href="/logo.svg" type="image/svg+xml" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
