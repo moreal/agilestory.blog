0cb761d983a7de741ddd0613e57d77a3972ae9e6
Lee Dogeon <dev.moreal@gmail.com>
2025-04-30 08:38:44 +0900

    feat: improve post navigation

--- components/PostNavigation.tsx
+++ components/PostNavigation.tsx
@@ -16,27 +16,31 @@ interface PostNavigationProps {
 export function PostNavigation({ prevPost, nextPost }: PostNavigationProps) {
   return (
     <nav class="w-full">
-      <ul class="flex flex-col justify-between w-full gap-4">
+      <ul class="flex flex-row justify-between w-full gap-4">
         {prevPost && (
-          <li class="w-full text-sm font-semibold text-left">
+          <li class="w-1/2 text-sm font-semibold text-left">
             <a
               href={`/${prevPost.id}`}
+              class="inline-block p-2 rounded hover:bg-gray-100 w-full"
             >
               <p>{"<"} {constants.PREVIOUS_POST_MESSAGE}</p>
-              <p class="text-gray-600">{prevPost.title}</p>
+              <p class="text-gray-600 break-words">{prevPost.title}</p>
             </a>
           </li>
         )}
+        {!prevPost && nextPost && <li class="w-1/2"></li>}
         {nextPost && (
-          <li class="w-full text-sm font-semibold text-right">
+          <li class="w-1/2 text-sm font-semibold text-right">
             <a
               href={`/${nextPost.id}`}
+              class="inline-block p-2 rounded hover:bg-gray-100 w-full"
             >
               <p>{constants.NEXT_POST_MESSAGE} {">"}</p>
-              <p class="text-gray-600">{nextPost.title}</p>
+              <p class="text-gray-600 break-words">{nextPost.title}</p>
             </a>
           </li>
         )}
+        {prevPost && !nextPost && <li class="w-1/2"></li>}
       </ul>
     </nav>
   );
